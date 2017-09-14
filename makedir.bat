437df4c19128d3fd96a85900029097c43b0a0e45
Austin Robbins <johnaustinrobbins@gmail.com>
2017-09-14 07:56:25 -0500

    Changed folder location to variable

--- makedir.bat
+++ makedir.bat
@@ -1,14 +1,15 @@
 @echo off
+set makedirpath=C:\File Preparation
 set arg1=%1
 IF %1.==. GOTO No1
 
-mkdir "C:\File Preparation\%arg1%\Applications"
-mkdir "C:\File Preparation\%arg1%\App Docs"
+mkdir "%makedirpath%\%arg1%\Applications"
+mkdir "%makedirpath%\%arg1%\App Docs"
 GOTO End1
 
 :No1
-mkdir "C:\File Preparation\test\Applications"
-mkdir "C:\File Preparation\test\App Docs"
+mkdir "%makedirpath%\test\Applications"
+mkdir "%makedirpath%\test\App Docs"
 GOTO End1
 
 
